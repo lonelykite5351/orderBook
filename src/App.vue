0d1e249f84ad5fe6adbea49a39d83637c42dbe75
lonelykite <lonelykite5351@gmail.com>
2023-07-15 01:43:41 +0800

    add WebSocket , methods setting

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <h1>123</h1>
+    <h1>{{ prevSeqNum }}</h1>
+    <h1>{{ seqNum }}</h1>
+    <button @click="start">start connect</button>
+    <button @click="send">send</button>
+    <button @click="stop">stop</button>
   </div>
 </template>
 
@@ -16,15 +20,54 @@ export default {
   },
   data() {
     return {
-      ws: null
+      ws: null,
+      seqNum: 0,
+      prevSeqNum: 0,
+      getData: null,
+      subscription: {
+        op: "subscribe",
+        args: ["update:BTC-PERP"],
+      },
+      // unsubscription: {
+      //   op: "unsubscribe",
+      //   args: ["update:BTC-PERP"],
+      // },
     };
   },
 
-  created(){
-    this.ws = new WebSocket("wss://ws.btse.com/ws/futures");
-    console.log(this.ws)
-    // this.ws.onopen
-  }
+  methods: {
+    start() {
+      // console.log(this.ws)
+      this.ws = new WebSocket("wss://ws.btse.com/ws/oss/futures");
+      console.log(this.ws);
+      this.ws.onopen = () => {};
+      this.ws.onmessage = (event) => {
+        const getData = JSON.parse(event.data).data;
+        if(getData){
+          console.log(getData)
+          this.seqNum = getData.seqNum
+          this.prevSeqNum = getData.prevSeqNum
+        }
+      };
+      this.ws.onclose = () => {
+        console.log("is Closed");
+      };
+    },
+    send() {
+      if(this.ws){
+        this.ws.send(JSON.stringify(this.subscription));
+      }
+    },
+    stop() {
+      if(this.ws){
+        this.ws.close()
+      }
+    },
+  },
+
+  created() {
+    this.start();
+  },
 };
 </script>
 
